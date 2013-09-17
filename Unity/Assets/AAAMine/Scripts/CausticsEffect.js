907543adbac7e1c4c85d6240011f02e1c473f70c
Federico Carrara <fede@federico-carraras-macbook.local>
2013-09-17 10:56:42 -0300

    caustics effect added.

---
+++ Unity/Assets/AAAMine/Scripts/CausticsEffect.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+// Change renderer's material each changeInterval
+// seconds from the material array defined in the inspector.
+var materials : Material[];
+var changeInterval = 0.2;
+
+function Update () {
+	if (materials.Length == 0) // do nothing if no materials
+		return;
+
+	// we want this material index now
+	var index : int = Time.time / changeInterval;
+	// take a modulo with materials count so that animation repeats
+	index = index % materials.Length;
+
+	// assign it to the renderer
+	renderer.sharedMaterial = materials[index];
+}
